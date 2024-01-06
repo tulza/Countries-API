149a11193859984a743c8f049ef2b18b5331078a
Tulza <40633228+tulza@users.noreply.github.com>
2024-01-07 00:39:32 +1300

    nav bar hover anim

--- src/Components/NavigationBar.jsx
+++ src/Components/NavigationBar.jsx
@@ -1,13 +1,22 @@
 import React, { useState } from "react";
 import { HandleTheme } from "../../ToggleTheme";
+import { motion } from "framer-motion";
 
 const NavigationBar = () => {
+  const hoverBar = { hover: { width: "100%" } };
   return (
     <div className="w-full h-[80px] bg-element px-[80px] flex items-center text-text justify-between shadow">
-      <p>Where in the world?</p>
-      <button className="p-4" onClick={HandleTheme}>
-        Dark Mode
-      </button>
+      <p className="font-bold">Where in the world?</p>
+      <motion.div whileHover="hover">
+        <button className="p-2 rounded-full" onClick={HandleTheme}>
+          Dark Mode
+        </button>
+        <motion.div
+          className="h-0.5 bg-text"
+          initial={{ width: 0 }}
+          variants={hoverBar}
+        />
+      </motion.div>
     </div>
   );
 };
